5fc589320719910b2fd48ab7091f47cf010e26eb
Walter Ceballo <waceballo@gmail.com>
2017-11-22 21:16:18 -0300

    + clase pair

--- ModeloDatos/src/model/Pair.java
+++ ModeloDatos/src/model/Pair.java
@@ -9,7 +9,8 @@ package model;
  *
  * @author alumno
  */
-public class Pair<L,R> {
+public class Pair<L, R> {
+
     private L left;
     private R right;
 
@@ -17,9 +18,31 @@ public class Pair<L,R> {
         this.left = left;
         this.right = right;
     }
-    
-    public L getLeft(){return left;}
-    public R getRight(){return right;}
 
+    public L getLeft() {
+        return left;
+    }
+
+    public R getRight() {
+        return right;
+    }
+
+    @Override
+    public int hashCode() {
+        return left.hashCode() ^ right.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Pair)) {
+            return false;
+        }
+        Pair pairo = (Pair) o;
+        return this.left.equals(pairo.getLeft())
+                && this.right.equals(pairo.getRight());
+    }
+
+    
+    
     
 }
